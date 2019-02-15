38b2374c6836cce6e5fbb045c3fba7b5d9366811
g17m3294 <g17m3294@campus.ru.ac.za>
2019-02-15 14:11:33 +0200

    problem7

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -6,10 +6,46 @@ using System.Threading.Tasks;
 
 namespace Euler
 {
+
     class Program
     {
+        static void prime()
+        {
+            int upperLimit = 10001;
+            int counter = 1;
+            bool isPrime;
+            int j;
+            List<int> primes = new List<int>();
+
+            primes.Add(2);
+
+            while (primes.Count < upperLimit)
+            {
+                counter += 2;
+                j = 0;
+                isPrime = true;
+                while (primes[j] * primes[j] <= counter)
+                {
+                    if (counter % primes[j] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                    j++;
+                }
+                if (isPrime)
+                {
+                    primes.Add(counter);
+                    Console.WriteLine("the 10 001st prime number is" + counter);
+                    Console.ReadLine();
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            prime();
+
         }
     }
 }
